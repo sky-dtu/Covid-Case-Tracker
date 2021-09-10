5c92718bafe424d6267d887604aeaa85363c4c52
Sandeep Yadav <sandeepkryadav1753@gmail.com>
2021-09-10 22:09:29 +0530

    Added First HTML Basic Structure

--- covid19-tracker/src/app/app.module.ts
+++ covid19-tracker/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ covid19-tracker/src/app/services/data-service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-31-2020.csv'
+  constructor( private http : HttpClient) { }
+
+  getGlobalData() {
+    this.http.get(this.globalDataUrl, {responseType : 'text'}).pipe {
+      map(result=>{
+        
+        let rows = result.split('\n');
+        console.log(rows);
+        
+
+        
+
+        return result;
+
+      })
+    }
+  }
+
+}
